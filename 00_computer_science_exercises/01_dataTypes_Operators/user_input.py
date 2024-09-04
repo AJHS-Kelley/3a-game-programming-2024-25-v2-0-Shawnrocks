db8269c56ac3cc0bfc0b53932e20a90f6b4bb72d
Shawnrocks <s12073904@students.duvalschools.org>
2024-09-04 13:20:24 -0400

    wrapper funcionts

--- 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -21,13 +21,13 @@ myNumber = int(input("Please enter an INTEGER number and press enter,\n"))
 # Wrapper Funcions
 # int() will convert the data to a INTEGER if possible.
 newNumber = input("Please type a value and press enter.\n")
-print(int(newNumber))
-print(float(newNumber))
-print(str(newNumber))
+print(int(newNumber)) # can convert STRING to INTEGER.
+print(float(newNumber)) # can convert STRING to FLOAT.
+print(str(newNumber)) # can convert INTEGER to STRING.
 
 # Float() will convert the data to a FLOAT if possible.
 newNumber = input("Please type a value and press enter.\n")
-print(int(newNumber)) # 
+#print(int(newNumber)) <-- cannot convert FLOAT to INTEGER 
 print(float(newNumber)) # can conert STRING to FLOAT.
 print(str(newNumber)) # can convert FLOAT to STRING.
 # str() will convert the data to a STRING if possible.
