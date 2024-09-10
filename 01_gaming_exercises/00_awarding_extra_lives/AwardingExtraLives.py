9fbd470f8f651cec20d78a0dc42318edbc0fa4c6
Shawnrocks <s12073904@students.duvalschools.org>
2024-09-10 13:21:31 -0400

    Awarding lives

---
+++ 01_gaming_exercises/00_awarding_extra_lives/AwardingExtraLives.py
@@ -0,0 +1,23 @@
+# Awarding Extra Lives, Tyshawn Ricks. v0.0
+
+
+lives = 3
+score = input("Please Enter Score")
+#Allow the user to input the score
+
+# If score is 10000 or less
+    # Lose a life
+# If score is > 10000 but less than 100001
+    # Give 1 extra life
+#If score is > 100000
+    #Give 2 extra lives
+
+# Output the score and number of lives to the screen.
+
+
+if score >= 100000:   
+    print("You receive 2 extra lives.\n")
+    
+
+
+
