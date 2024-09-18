5e12ef65995c6ddb3d1d9ff76f23d844bebb2641
Shawnrocks <s12073904@students.duvalschools.org>
2024-09-18 13:15:41 -0400

    v0.3

--- 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.py
@@ -16,7 +16,50 @@ cpuChoice = None
 playerName = input("Please type you name and press enter.\n")
 print(f"Hello {playerName}!\n")
 isCorrect = input("is that correct? Type yes or no and press enter.\n")
-if isCorrect == "yes":
+if isCorrect == "yes".lower():
     print(f"Ok {playerChoice}, let's play Rock, Paper, Scissors!\n")
 else:
     playerName = input("Please type you name and press enter.\n")
+
+
+
+
+
+# The rules
+print("""
+Welcome to the Rock, paper, scissor robot!
+it's time to play rock, paper, scissors
+      
+you will play against the CPU. The first player to score 5 points wins.
+You will select from rock, paper, scissors!
+The CPU will select rock, paper, scissors at random.
+      
+1) ROCK BEATS SCISSORS
+2) SCISSOR BEATS PAPER
+3) PAPER BEATS ROCK
+""")     
+
+# MULTI-LINE STRINGS CAN BE USED AS BIG COMMENTS
+"""" YOu can put comments in these strings aswell """
+
+
+
+
+
+#Main game loop
+while playerScore < 5 and cpuScore < 5:
+    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
+    playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+    playerChoice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
+        
+
+
+
+
+    # print the current score 
+    # let player select rock, paper, scissor
+    # let cpu select choice at random
+    # compare player choice to cpu choice
+    # print the results to the screen
+    # award point to the winner and output results
