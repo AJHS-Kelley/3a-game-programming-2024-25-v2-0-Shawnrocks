fa7d086af3e6c780c28b74f71659b53aebd1dba5
Shawnrocks <s12073904@students.duvalschools.org>
2024-09-18 11:49:54 -0400

    RPS v0.1

---
+++ 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.py
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors by Tyshawn Ricks, v0.1
+
+# Module Imports
+import random
+
+# DATA STRUCTURES -- player
+playerScore = 0
+playerName = "Test Player"
+playerChoice = None 
+
+# Data Structures -- CPU
+cpuScore = 0
+cpuChoice = None
