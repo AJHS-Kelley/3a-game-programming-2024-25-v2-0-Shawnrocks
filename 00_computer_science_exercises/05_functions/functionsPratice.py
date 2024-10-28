7cb95f9563812c29f8b6fdab113682d96b73208e
Shawnrocks <s12073904@students.duvalschools.org>
2024-10-28 13:14:22 -0400

    yes

--- 00_computer_science_exercises/05_functions/functionsPratice.py
+++ 00_computer_science_exercises/05_functions/functionsPratice.py
@@ -22,6 +22,29 @@ def helloWorldMulti(): # FUNCTION SIGNATURE
     elif language == "chinese":
         print("世界您好")
     else:
-        exit()
+        print("FAILURE")
 
-helloWorldMulti()
+helloWorldMulti() # FUNCTION CALL
+
+# Function to determine even / odd numbers
+argument1 = random.randint(-1000, 1000)
+
+def isEven(argument1: int) -> bool: #Requires one argument (argument1) and RETURNS a boolean value
+    """Determines if an integer value is even or odd."""
+    if argument1 % 2 == 0:
+        return True
+    else:
+        return False
+    
+print(isEven(argument1))
+
+# Function with Multiple Arguments
+def canRideRollerCoaster(age: int, height: int) -> bool:
+    if age > 10 and height > 4:
+        print("You can ride.\n")
+        return True
+    else:
+        print("You cannot ride.\n")
+        return False
+
+canRideRollerCoaster(4, 10) # Arguments must be passed in the same order as the function signature indicates.
